2de5e7629aad0fd54c2185fe3187b2974b907f88
Dariohg <rubendarioh99@gmail.com>
2024-09-26 00:39:54 -0600

    Perritos pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/organisms/header/header.component';
-<<<<<<< HEAD
-=======
 import { FooterComponent } from './components/organisms/footer/footer.component';
->>>>>>> d544c7cc5103d2628e8fa7950404ca0238aeba1a
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,15 +8,9 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
-<<<<<<< HEAD
-  `,
-  standalone: true,
-  imports: [HeaderComponent, RouterOutlet]
-=======
     <app-footer></app-footer>
   `,
   standalone: true,
   imports: [HeaderComponent, FooterComponent, RouterOutlet]
->>>>>>> d544c7cc5103d2628e8fa7950404ca0238aeba1a
 })
 export class AppComponent {}
